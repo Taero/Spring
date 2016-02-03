c3a0f8734f950da640b301bbc873348cbcb10df7
Artur Lukaszczyk <artur.lukaszczyk@capgemini.com>
2016-02-03 18:17:56 +0100

    Po pierwszym zadanku

--- web/src/main/java/pl/spring/demo/web/controller/BookController.java
+++ web/src/main/java/pl/spring/demo/web/controller/BookController.java
@@ -21,4 +21,6 @@ public class BookController {
         params.put("books", allBooks);
         return "bookList";
     }
+    
+    
 }
--- web/src/main/java/pl/spring/demo/web/jetty/EmbeddedJetty.java
+++ web/src/main/java/pl/spring/demo/web/jetty/EmbeddedJetty.java
@@ -1,4 +1,4 @@
-package pl.spring.demo.web.jetty;
+ package pl.spring.demo.web.jetty;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
